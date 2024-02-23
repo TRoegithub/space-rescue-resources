5a07d2a7801d5a41f94604ade951fcf813b6252f
unknown <26RoebuT@mbbc.qld.edu.au>
2024-02-23 14:40:45 +1000

    Added scoring

--- Rooms/GamePlay.py
+++ Rooms/GamePlay.py
@@ -1,6 +1,7 @@
-from GameFrame import Level
+from GameFrame import Level, Globals
 from Objects.Ship import Ship
 from Objects.Zork import Zork
+from Objects.Hud import Score
 
 class GamePlay(Level):
     def __init__(self, screen, joysticks):
@@ -9,6 +10,12 @@ class GamePlay(Level):
         # set background image
         self.set_background_image("Background.png")
 
-         # add objects
+        # add objects
         self.add_room_object(Ship(self, 25, 50))
         self.add_room_object(Zork(self,1120, 50))
+
+        # add HUD items
+        self.score = Score(self, 
+                           Globals.SCREEN_WIDTH/2 - 20, 20, 
+                           str(Globals.SCORE))
+        self.add_room_object(self.score)
